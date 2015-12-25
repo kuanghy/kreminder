e40a05c041fb24738b01ad396fe61ade40e49e29
huoty <sudohuoty@163.com>
2015-12-25 17:17:26 +0800

    first commit

---
+++ kigh.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# *************************************************************
+#     Filename @  kigh.py
+#       Author @  Huoty
+#  Create date @  2015-12-25 16:59:16
+#  Description @  
+# *************************************************************
+
+import os
+
+# Script starts from here
+
+if __name__ == "__main__":
+    os.system('notify-send "rsnapshot done :)" ')
